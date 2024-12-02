a7f83559271684ba976c72cd0ab917fcc985f147
Alejandro Ruiz <aruizsumelzo@deloitte.es>
2024-12-02 12:12:47 +0100

    Add routing just to encap

---
+++ app/logo/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Logo3D from "@/components/logo-3d/Logo3D";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Logo3D />
+    </div>
+  );
+}

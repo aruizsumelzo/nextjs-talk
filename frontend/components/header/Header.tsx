ffe9bb6f9c32c905fd9079390a909aad76cbc282
Alejandro Ruiz <aruizsumelzo@deloitte.es>
2024-12-10 12:46:31 +0100

    Add fix to TS complain

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -1,10 +1,11 @@
-import { Suspense } from "react";
-import { HeaderSkeleton } from "@/components/header/header-skeleton";
-import { HeaderContent } from "@/components/header/header-content";
+import { Suspense } from 'react';
+import { HeaderSkeleton } from '@/components/header/header-skeleton';
+import { HeaderContent } from '@/components/header/header-content';
 
 export function Header() {
   return (
     <Suspense fallback={<HeaderSkeleton />}>
+      {/* @ts-expect-error Server Component */}
       <HeaderContent />
     </Suspense>
   );

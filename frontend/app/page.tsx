910ec916f101725930d9e825b4c728b38beb080c
Alejandro Ruiz <aruizsumelzo@deloitte.es>
2024-12-10 16:05:37 +0100

    Delete explanating pages

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,18 +10,6 @@ export default function HomePage() {
       </h1>
 
       <div className="flex flex-col items-center gap-6 w-full max-w-sm">
-        <Link href="/rendering" passHref>
-          <Button className="px-5 py-3 text-lg font-medium text-white bg-black rounded-md shadow hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-[#86BC25] transition-all">
-            Ejemplo de SSR
-          </Button>
-        </Link>
-
-        <Link href="/images" passHref>
-          <Button className="px-5 py-3 text-lg font-medium text-white bg-black rounded-md shadow hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-[#86BC25] transition-all">
-            Ejemplos de componentes
-          </Button>
-        </Link>
-
         <Link href="/shop" passHref>
           <Button className="w-full bg-[#86BC25] text-white hover:bg-[#74a71f]">
             Ir a la tienda
